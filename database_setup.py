1fea2d4ea2d02be577e816b8c1ffea339338802e
Vincent Din <vincent.din@colorado.edu>
2017-12-17 05:16:59 -0700

    implemented cascade property (delete children items when a parent category is deleted)

--- database_setup.py
+++ database_setup.py
@@ -29,6 +29,7 @@ class CatalogCategory(Base):
     name = Column(String(250), nullable=False)
     user_id = Column(Integer,ForeignKey('user.id'))
     user = relationship(User)
+    items = relationship("CatalogItem", cascade="all, delete-orphan")
 
     @property
     def serialize(self):
